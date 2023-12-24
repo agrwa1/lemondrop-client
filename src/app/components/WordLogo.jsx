9d31d6417b67ba6e24b4c9ee49dfde18564fc71a
Sohum Agrawal <sohum@Sohums-Air.attlocal.net>
2023-12-24 00:40:47 -0800

    changed logo to have graphic and word

--- src/app/components/WordLogo.jsx
+++ src/app/components/WordLogo.jsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import logo from '../../../public/ld-word-logo-1.png'
+import logo from '../../../public/ld-logo-yellow.png'
 // import logo from '../../../public/ld-logo-all-white.png'
 
 export default function WordLogo() {
 	return (
-		<Image src={logo} height={30} alt="Lemondrop Logo" />
+		<Image src={logo} height={45} alt="Lemondrop Logo" />
 		// <div className="flex flex-col">
 		// 	<h2 className="font-logo m-0 p-0 font-bold text-3xl text-yellow" >lemondrop</h2>
 		// 	<h2 className="m-0 p-0 text-xl " >SPORTSBOOK</h2>
