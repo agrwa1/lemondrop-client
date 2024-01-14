011115a996e7d343c36172b0776c9c4db8e7cf01
Sohum Agrawal <sohum@sohums-air.3521.dhcp.asu.edu>
2024-01-13 17:01:13 -0700

    shrunk lemondrop logo

--- src/app/components/WordLogo.jsx
+++ src/app/components/WordLogo.jsx
@@ -4,7 +4,7 @@ import logo from '../../../public/ld-logo-yellow.png'
 
 export default function WordLogo() {
     return (
-        <Image src={logo} height={50} alt="Lemondrop Logo" />
+        <Image src={logo} height={40} alt="Lemondrop Logo" />
         // <div className="flex flex-col">
         // 	<h2 className="font-logo m-0 p-0 font-bold text-3xl text-yellow" >lemondrop</h2>
         // 	<h2 className="m-0 p-0 text-xl " >SPORTSBOOK</h2>
